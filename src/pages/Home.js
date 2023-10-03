d1af5b37499f0dea70619208ac359be4d94fdc22
Izabel de Barros <izabelfeigel@gmail.com>
2023-10-03 13:40:27 -0300

    FEAT: Loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,19 @@ import "./Home.css";
 export default function Home() {
   const [query, setQuery] = useState();
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
 
   const fetchSearch = async () => {
+    if (!data) {
+        setLoading(true);
+    }
+
     const response = await fetch(
       `https://openlibrary.org/search.json?q=${query}`
     );
     const json = await response.json();
     setData(json);
+    setLoading(false);
   };
 
   return (
@@ -29,6 +35,7 @@ export default function Home() {
       </div>
 
       <div>
+        {loading && <p>Loading...</p>}
         {data &&
           data.docs.map((info) => (
             <div key={info.key} className="search-card">
